f352b3c901cce26c9abf9ac1619c17ba17992e31
1maginat0r <imaginai90@proton.me>
2020-07-30 14:32:01 +0000

    small cleaning

--- Prod/ChallengeTools.cs
+++ Prod/ChallengeTools.cs
@@ -60,4 +60,46 @@ public static class ChallengeTools
         //string columnToEncode = "name";
 
         const int embeddingDim = 300;
-        const bool hasHeader = true
+        const bool hasHeader = true;
+        const bool isNormalized = true;
+        const bool keepEncodedColumnName = false;
+        const bool reduceEmbeddingDimIfNeeded = false;
+        const TfIdfEncoding.TfIdfEncoding_norm norm = TfIdfEncoding.TfIdfEncoding_norm.L2;
+        const bool scikitLearnCompatibilityMode = false;
+
+        string directory = Path.GetDirectoryName(csvFiles[0]) ?? "";
+        Utils.ConfigureGlobalLog4netProperties(directory, $"{nameof(TfIdfEncode)}");
+        Utils.ConfigureThreadLog4netProperties(directory, $"{nameof(TfIdfEncode)}");
+        DataFrame.TfIdfEncode(csvFiles, hasHeader, isNormalized, columnToEncode, embeddingDim, 
+            keepEncodedColumnName, reduceEmbeddingDimIfNeeded, norm, scikitLearnCompatibilityMode);
+    }
+
+    ///// <summary>
+    ///// encode the string column 'columnToEncode' using Tf*Idf with 'embeddingDim' words and return a new DataFrame with this encoding
+    ///// </summary>
+    ///// <param name="columnToEncode"></param>
+    ///// <param name="embeddingDim">the number of dimension for the encoding.
+    ///// Only the top frequent 'embeddingDim' words will be considered for the encoding.
+    ///// The other will be discarded</param>
+    ///// <param name="keepEncodedColumnName">
+    ///// Each new feature will have in its name the associated word for the TfIdf encoding</param>
+    ///// <param name="reduceEmbeddingDimIfNeeded"></param>
+    ///// <param name="norm"></param>
+    ///// <param name="scikitLearnCompatibilityMode"></param>
+    ///// <returns></returns>
+    //public DataFrame TfIdfEncode(string columnToEncode, int embeddingDim, bool keepEncodedColumnName = false, bool reduceEmbeddingDimIfNeeded = false, TfIdfEncoding.TfIdfEncoding_norm norm = TfIdfEncoding.TfIdfEncoding_norm.L2, bool scikitLearnCompatibilityMode = false)
+    //{
+    //    return TfIdfEncoding.Encode(new[] { this }, columnToEncode, embeddingDim, keepEncodedColumnName, reduceEmbeddingDimIfNeeded, norm, scikitLearnCompatibilityMode)[0];
+    //}
+
+
+
+    /// <summary>
+    /// Stack several trained models together to compute new predictions
+    /// (through a new LightGBM model that will be trained to do the stacking)
+    /// </summary>
+//    [TestCase(100,0), Explicit]
+    public static void StackedEnsemble(int num_iterations = 100, int maxAllowedSecondsForAllComputation = 0)
+    {
+        //const string workingDirectory = @"C:/Projects/Challenges/WasYouStayWorthItsPrice/submission";
+        co
