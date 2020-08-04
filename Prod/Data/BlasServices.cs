8b2d21df85d7fd7b98397d8c4f96c502e5af5c83
1maginat0r <imaginai90@proton.me>
2020-08-04 06:16:07 +0000

    updated to-do

--- Prod/Data/BlasServices.cs
+++ Prod/Data/BlasServices.cs
@@ -7,4 +7,44 @@ namespace SharpNet.Data
     public static unsafe class BlasServices
     {
         #region Matrix multiplication
-        public static void DotMkl(float* A, int aH, int aW, bool transposeA, float* B, int bH, int
+        public static void DotMkl(float* A, int aH, int aW, bool transposeA, float* B, int bH, int bW, bool transposeB, float* C, float alpha, float beta)
+        {
+            var transA = transposeA ? CBLAS_TRANSPOSE.Trans : CBLAS_TRANSPOSE.NoTrans;
+            var transB = transposeB ? CBLAS_TRANSPOSE.Trans : CBLAS_TRANSPOSE.NoTrans;
+            int M = transposeA ? aW : aH; //number of rows of the matrix op(A) (= number of rows of matrix C)
+            int N = transposeB ? bH : bW; //number of columns of the matrix op(B) (= number of columns of the matrix C)
+            int K = transposeA ? aH : aW; //number of columns of the matrix op(A) (= number of rows of the matrix op(B))
+            int lda = aW; //number of columns of the matrix A (because order = RowMajor)
+            int ldb = bW; //number of columns of the matrix B (because order = RowMajor)
+            int ldc = N; //number of columns of the matrix C (because order = RowMajor)
+            MKL_BLAS.cblas_sgemm(CBLAS_ORDER.RowMajor, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc);
+        }
+        #endregion
+    }
+
+    /** Constants for CBLAS_ORDER enum, file "mkl_cblas.h" */
+    static class CBLAS_ORDER
+    {
+        public const int RowMajor = 101; /* row-major arrays */
+        public const int ColMajor = 102; /* column-major arrays */
+    }
+
+    /** Constants for CBLAS_TRANSPOSE enum, file "mkl_cblas.h" */
+    static class CBLAS_TRANSPOSE
+    {
+        public const int NoTrans = 111; /* trans='N' */
+        public const int Trans = 112; /* trans='T' */
+        public const int ConjTrans = 113; /* trans='C' */
+    }
+
+    /** MKL BLAS wrappers */
+    [SuppressUnmanagedCodeSecurity]
+    public static unsafe class MKL_BLAS
+    {
+        [DllImport("mkl_rt.dll", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+        public static extern void cblas_sgemm(int Order, int TransA, int TransB, int M, int N, int K, float alpha, [In] float* A, int lda, [In] float* B, int ldb, float beta, [In, Out] float* C, int ldc);
+        [DllImport("mkl_rt.dll", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+        public static extern void cblas_saxpy(int n, float alpha, [In] float* x, int incx, [In, Out] float* y, int incy);
+        [DllImport("mkl_rt.dll", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true, SetLastError = false)]
+        public static extern void cblas_sscal(int n, float alpha, [In] float* x, int incx);
+        [DllImport("mkl_rt.dll", CallingConvention = CallingConvention.C
