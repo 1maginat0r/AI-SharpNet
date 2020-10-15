03318dff79578008c69f70b155130ba71951d4e9
1maginat0r <imaginai90@proton.me>
2020-10-15 22:59:58 +0000

    - Bumped the version number

--- Prod/Datasets/Biosonar85/Biosonar85Utils.cs
+++ Prod/Datasets/Biosonar85/Biosonar85Utils.cs
@@ -8,4 +8,53 @@ using SharpNet.CPU;
 using SharpNet.Data;
 using SharpNet.DataAugmentation;
 using SharpNet.GPU;
-using Shar
+using SharpNet.HPO;
+using SharpNet.Hyperparameters;
+using SharpNet.LightGBM;
+using SharpNet.Networks;
+using SharpNet.Networks.Transformers;
+using static SharpNet.Networks.NetworkSample;
+
+namespace SharpNet.Datasets.Biosonar85;
+
+public static class Biosonar85Utils
+{
+    public const string NAME = "Biosonar85";
+    public static string WorkingDirectory => Path.Combine(Utils.ChallengesPath, NAME);
+    public static string DataDirectory => Path.Combine(WorkingDirectory, "Data");
+
+    public static void Run()
+    {
+        Utils.ConfigureGlobalLog4netProperties(WorkingDirectory, NAME, true);
+        Utils.ConfigureThreadLog4netProperties(WorkingDirectory, NAME, true);
+
+        //Mixup.DisplayStatsForAlphaMixup();return;
+
+        //var trainDataset = Load("X_train_23168_101_64_1024_512.bin", "Y_train_23168_1_64_1024_512.bin", true);
+        //var testDataset = Load("X_test_950_101_64_1024_512.bin", "Y_test_950_1_64_1024_512.bin", false);
+
+        //var bin_file = Path.Combine(DataDirectory, "Y_train_ofTdMHi.csv.bin");
+        //var tensor = CpuTensor<float>.LoadFromBinFile(bin_file, new[] { -1, 101, 64});
+        //ParseLogFile(); return;
+
+        //Log.Info(AllCombinations(0.4, 0.7));return;
+        //Log.Info(AllCombinations(0.701, 0.85));return;
+
+        //ChallengeTools.Retrain(Path.Combine(WorkingDirectory, "Dump"), "2A4F619211", null, percentageInTraining:0.5, retrainOnFullDataset:false, useAllAvailableCores:true);return;
+        //ChallengeTools.Retrain(Path.Combine(WorkingDirectory, "Dump"), "FCB789043E_18", null, percentageInTraining: 0.5, retrainOnFullDataset: false, useAllAvailableCores: true); return;
+        //ChallengeTools.Retrain(Path.Combine(WorkingDirectory, "Dump"), "9811DDD19E_19", null, percentageInTraining: 0.5, retrainOnFullDataset: false, useAllAvailableCores: true); return;
+        //ChallengeTools.Retrain(Path.Combine(WorkingDirectory, "Dump"), "E5BA77E393", null, percentageInTraining: 0.5, retrainOnFullDataset: false, useAllAvailableCores: true); return;
+
+
+        //ChallengeTools.ComputeAndSaveFeatureImportance(WorkingDirectory, "674CD08C52", true); return;
+
+
+        //ComputeAverage_avg();return;
+
+        //Launch_HPO_spectrogram(20); return;
+
+        //Launch_HPO_MEL_SPECTROGRAM_256_801_BinaryCrossentropy(20); return;
+        Launch_HPO_MEL_SPECTROGRAM_256_801_BCEWithFocalLoss(20); return;
+        //Launch_HPO_MEL_SPECTROGRAM_64_401(10); return;
+        //Launch_HPO_MEL_SPECTROGRAM_SMALL_128_401(10); return;
+        //Launch_HPO_MEL_SPECTROGRAM_128_401_BinaryCrossentropy(20); ret
