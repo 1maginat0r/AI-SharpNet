fb1241843534097db7218a35feed65a288312279
1maginat0r <imaginai90@proton.me>
2020-10-17 05:48:04 +0000

    Add Brewfile

--- Prod/Datasets/Biosonar85/Biosonar85Utils.cs
+++ Prod/Datasets/Biosonar85/Biosonar85Utils.cs
@@ -57,4 +57,61 @@ public static class Biosonar85Utils
         Launch_HPO_MEL_SPECTROGRAM_256_801_BCEWithFocalLoss(20); return;
         //Launch_HPO_MEL_SPECTROGRAM_64_401(10); return;
         //Launch_HPO_MEL_SPECTROGRAM_SMALL_128_401(10); return;
-        //Launch_HPO_MEL_SPECTROGRAM_128_401_BinaryCrossentropy(20); ret
+        //Launch_HPO_MEL_SPECTROGRAM_128_401_BinaryCrossentropy(20); return;
+        //Launch_HPO_MEL_SPECTROGRAM_128_401_BCEWithFocalLoss(10); return;
+
+        //LaunchCatBoostHPO(1000); return;
+
+        //LaunchLightGBMHPO(350);return;
+
+        //Launch_HPO_Transformers(10); return;
+        //Launch_HPO(10);return;
+        //OptimizeModel_AE0F13543C();
+    }
+
+
+    public static void OptimizeModel_AE0F13543C()
+    {
+        var eachUpdate = new List<Tuple<string, string>>
+                         {
+                             Tuple.Create("HeightShiftRangeInPercentage","0.03"),
+                             Tuple.Create("TopDropoutRate","0.2"),
+                             Tuple.Create("HeightShiftRangeInPercentage","0.1"),
+                             Tuple.Create("SkipConnectionsDropoutRate","0.5"),
+                             Tuple.Create("SkipConnectionsDropoutRate","0.3"),
+                             Tuple.Create("AlphaMixup","1"),
+                             Tuple.Create("InitialLearningRate","0.002"),
+                             Tuple.Create("InitialLearningRate","0.003"),
+                             Tuple.Create("CutoutPatchPercentage","0.1"),
+                             Tuple.Create("CutoutPatchPercentage","0.2"),
+                             Tuple.Create("RowsCutoutPatchPercentage","0.06"),
+                             Tuple.Create("RowsCutoutPatchPercentage","0.18"),
+                             //Tuple.Create("TopDropoutRate","0.4"), //done
+                         };
+        void ContentUpdate(string key, string newValue, IDictionary<string, string> content)
+        {
+            if (content.ContainsKey(key))
+            {
+                content[key] = newValue;
+            }
+        }
+
+        foreach(var (key, newValue) in eachUpdate)
+        {
+            ISample.Log.Info($"Updating {key} to {newValue}");
+            ChallengeTools.RetrainWithContentUpdate(Path.Combine(WorkingDirectory, "OptimizeModel", "1EF57E45FC_16"), "1EF57E45FC_16", a => ContentUpdate(key, newValue, a));
+        }
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static string AllCombinations(double minPercentage, double maxPercentage)
+    {
+        var siteToCount = SiteToCount();
+
+        double totalCount = siteToCount.Select(m => m.Value).Sum();
+        List<Tuple<string, double>> allSites = new();
+        foreach( var (site,count) in siteToCount)
+        {
+            allSites.Add(Tuple.Create(site,count/totalCount));
+        }
+        allSites = allSites.OrderByDescending(s =>
