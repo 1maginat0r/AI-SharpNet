7a7fc2f56ea83a696e2c243b09c4575e6960dee4
1maginat0r <imaginai90@proton.me>
2021-06-27 09:33:31 +0000

    udf: Avoid infinite loop when processing indirect ICBs

--- Prod/Datasets/CFM60/CFM60Entry.cs
+++ Prod/Datasets/CFM60/CFM60Entry.cs
@@ -44,4 +44,61 @@ namespace SharpNet.Datasets.CFM60
             {
                 if (double.TryParse(splitted[index++], out var tmp))
                 {
-  
+                    rel_vol[i] = (float)tmp;
+                }
+            }
+            LS = (float)double.Parse(splitted[index++], CultureInfo.InvariantCulture);
+            NLV = (float)double.Parse(splitted[index], CultureInfo.InvariantCulture);
+        }
+
+        [ProtoMember(1)]
+        public int ID { get; set; }
+        [ProtoMember(2)]
+        public int pid { get; set; }
+        [ProtoMember(3)]
+        public int day { get; set; }
+        [ProtoMember(4)]
+        public float[] abs_ret { get; set; }
+        [ProtoMember(5)]
+        public float[] rel_vol{ get; set; }
+        [ProtoMember(6)]
+        public float LS { get; set; }
+        [ProtoMember(7)]
+        public float NLV { get; set; }
+        [ProtoMember(8)] 
+        public float Y { get; set; } = float.NaN;
+        
+        public static CFM60Entry[] Load(string xFile, string yFileIfAny, Action<string> log)
+        {
+            Debug.Assert(xFile != null);
+
+            var protoBufFile = xFile + ".proto";
+            if (File.Exists(protoBufFile))
+            {
+                log("Loading ProtoBuf file " + protoBufFile + "...");
+                using var fsLoad = new FileStream(protoBufFile, FileMode.Open, FileAccess.Read);
+                var result = Serializer.Deserialize<CFM60Entry[]>(fsLoad);
+                log("Binary file " + protoBufFile + " has been loaded");
+                return result;
+            }
+
+            log("Loading content of file " + xFile + "...");
+            var xFileLines = File.ReadAllLines(xFile);
+
+            log("File " + xFile + " has been loaded: " + xFileLines.Length + " lines");
+
+            log("Parsing lines of file " + xFile + "...");
+            var entries = new CFM60Entry[xFileLines.Length - 1];
+            var idToIndex = new ConcurrentDictionary<int, int>();
+            void ProcessLine(int i)
+            {
+                Debug.Assert(i >= 1);
+                entries[i - 1] = new CFM60Entry(xFileLines[i]);
+                idToIndex[entries[i - 1].ID] = i - 1;
+            }
+            System.Threading.Tasks.Parallel.For(1, xFileLines.Length, ProcessLine);
+            log("Lines of file " + xFile + " have been parsed");
+            foreach (var (id, y) in CFM60DatasetSample.LoadPredictions(yFileIfAny))
+            {
+                entries[idToIndex[id]].Y = (float)y;
+            }
