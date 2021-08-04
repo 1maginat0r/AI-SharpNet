362b0f7ba1d80b465bdde399146270aef65ed18c
1maginat0r <imaginai90@proton.me>
2021-08-04 19:03:58 +0000

    dash

--- Prod/Datasets/EffiSciences95/EffiSciences95DirectoryDataSet.cs
+++ Prod/Datasets/EffiSciences95/EffiSciences95DirectoryDataSet.cs
@@ -30,4 +30,69 @@ public class EffiSciences95DirectoryDataSet : DirectoryDataSet
 
         for(int pictureId=0;pictureId<=EffiSciences95Utils.MaxPictureId(directory);++pictureId)
         {
-            var labelCoordinatesIn
+            var labelCoordinatesInPicture = idToLabelCoordinatesInPicture.TryGetValue(pictureId, out var tmp) ? tmp : null;
+            if (labelCoordinatesInPicture!= null && (labelCoordinatesInPicture.IsEmpty || !labelCoordinatesInPicture.HasKnownLabel))
+            {
+                labelCoordinatesInPicture = null;
+            }
+            // for Labeled (train) dataset:
+            //    0 or 1
+            // for Unlabeled (test) dataset:
+            //    -1
+            int label = -1; 
+            if (directory=="Labeled")
+            {
+                if (labelCoordinatesInPicture == null)
+                {
+                    continue; //for training, we need to know the box shape for each picture to remove it while training
+                }
+                // we extract the label for this training pictureId
+                label = pictureId_to_TrainLabel[pictureId];
+            }
+            elementIdToPaths.Add(new List<string> { EffiSciences95Utils.PictureIdToPath(pictureId, directory) });
+            elementId_to_pictureId.Add(pictureId.ToString());
+            elementId_to_label.Add(label);
+            allLabelCoordinatesInPictures.Add(labelCoordinatesInPicture);
+            if (maxElementCount != -1 && elementIdToPaths.Count >= maxElementCount)
+            {
+                break;
+            }
+        }
+
+        return new EffiSciences95DirectoryDataSet(
+            datasetSample,
+            allLabelCoordinatesInPictures,
+            elementIdToPaths,
+            elementId_to_pictureId,
+            elementId_to_label);
+    }
+
+    private EffiSciences95DirectoryDataSet(
+        EffiSciences95DatasetSample datasetSample,
+        List<EffiSciences95LabelCoordinates> labelCoordinates,
+        List<List<string>> elementIdToPaths,
+        List<string> y_IDs,
+        List<int> elementIdToCategoryIndex
+    )
+        : base(
+            datasetSample,
+            elementIdToPaths, 
+            elementIdToCategoryIndex, 
+            null,
+            EffiSciences95Utils.NAME, 
+            PrecomputedMeanAndVolatilityForEachChannel,
+            ResizeStrategyEnum.None, 
+            null,
+            y_IDs.ToArray())
+    {
+        _datasetSample = datasetSample;
+        _labelCoordinates = labelCoordinates;
+    }
+
+
+
+    /// <summary>
+    /// return a picture (from the training (Labeled) or test (Unlabeled) dataset) after removing the box that seems to contain the label 
+    /// </summary>
+    /// <returns></returns>
+    public override BitmapContent OriginalElementCo
