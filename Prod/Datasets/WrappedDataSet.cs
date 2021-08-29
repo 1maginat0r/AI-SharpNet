0ff251308b56c4c961fb1de6bd57baa37f44416d
1maginat0r <imaginai90@proton.me>
2021-08-29 17:07:46 +0000

    added favicon.ico

--- Prod/Datasets/WrappedDataSet.cs
+++ Prod/Datasets/WrappedDataSet.cs
@@ -14,4 +14,45 @@ public abstract class WrappedDataSet : DataSet
             original.DatasetSample,
             original.MeanAndVolatilityForEachChannel,
             original.ResizeStrategy,
-            orig
+            original.ColumnNames,
+            Y_IDs,
+            original.Separator)
+    {
+        _original = original;
+    }
+
+    public override string ToString()
+    {
+        return _original.ToString();
+    }
+
+    public override void LoadAt(int subElementId, int indexInBuffer, CpuTensor<float> xBuffer, CpuTensor<float> yBuffer, bool withDataAugmentation, bool isTraining)
+    {
+        _original.LoadAt(subElementId, indexInBuffer, xBuffer, yBuffer, withDataAugmentation, isTraining);
+    }
+    public override int Count => _original.Count;
+    public override int[] X_Shape(int batchSize) => _original.X_Shape(batchSize);
+    public override int[] Y_Shape(int batchSize) => _original.Y_Shape(batchSize);
+    public override int ElementIdToCategoryIndex(int elementId) => _original.ElementIdToCategoryIndex(elementId);
+    public override bool CanBeSavedInCSV => _original.CanBeSavedInCSV;
+    public override bool UseRowIndexAsId => _original.UseRowIndexAsId;
+    public override List<int[]> XMiniBatch_Shape(int[] shapeForFirstLayer) => _original.XMiniBatch_Shape(shapeForFirstLayer);
+    public override int[] IdToValidationKFold(int n_splits, int countMustBeMultipleOf)
+    {
+        if (Count != _original.Count)
+        {
+            throw new ArgumentException($"{Count} != {_original.Count}");
+        }
+        return _original.IdToValidationKFold(n_splits, countMustBeMultipleOf);
+    }
+
+    #region Dispose pattern
+    protected override void Dispose(bool disposing)
+    {
+        Disposed = true;
+#pragma warning disable CA1816
+        GC.SuppressFinalize(this);
+#pragma warning restore CA1816
+    }
+    #endregion
+}
---
+++ Prod/Datasets/WrappedDatasetSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SharpNet.Datasets;
+
+public abstract class WrappedDatasetSample : AbstractDatasetSample
+{
+    protected AbstractDatasetSample Original { get; }
+
+    protected WrappedDatasetSample(AbstractDatasetSample original)
+    {
+        Original = original;
+    }
+
+    public override bool IsCategoricalColumn(string columnName) => Original.IsCategoricalColumn(columnName);
+    public override string IdColumn => Original.IdColumn;
+    public override string[] TargetLabels => Original.TargetLabels;
+    public override Objective_enum GetObjective() => Original.GetObjective();
+    public override DataFrame Predictions_InModelFormat_2_Predictions_InTargetFormat(DataFrame predictions_InModelFormat, Objective_enum objective) => Original.Predictions_InModelFormat_2_Predictions_InTargetFor
