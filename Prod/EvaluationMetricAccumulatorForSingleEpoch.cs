a43a28a9925cfe91eb01202720732e1e7051bc64
1maginat0r <imaginai90@proton.me>
2021-08-29 22:53:41 +0000

    Reduce font size

--- Prod/EvaluationMetricAccumulatorForSingleEpoch.cs
+++ Prod/EvaluationMetricAccumulatorForSingleEpoch.cs
@@ -46,4 +46,64 @@ public class EvaluationMetricAccumulatorForSingleEpoch : IDisposable
             yPredicted = yPredicted.Reshape(newShape);
         }
 
-        if 
+        if (_metricData.Metrics.Any(RequireFullYToCompute))
+        {
+            //we'll just update the tensors '_full_y_true' and '_full_y_pred' by appending to those tensors
+            //the content of the tensors 'yExpected' and 'yPredicted'
+            if (_full_y_true == null)
+            {
+                var full_y_true_shape = Utils.CloneShapeWithNewCount(yExpected.Shape, _count);
+                _full_y_true = _memoryPool.GetFloatTensor(full_y_true_shape);
+                _full_y_pred = _memoryPool.GetFloatTensor(_full_y_true.Shape);
+            }
+           
+            Debug.Assert(yExpected.Shape[0] + _currentElementCount <= _count);
+            yExpected.CopyTo(_full_y_true.Slice(_currentElementCount, yExpected.Shape));
+            yPredicted.CopyTo(_full_y_pred.Slice(_currentElementCount, yPredicted.Shape));
+        }
+        else
+        {
+            Tensor buffer = null;
+            foreach (var metric in _metricData.Metrics)
+            {
+                if (!_currentAccumulatedMetrics.ContainsKey(metric))
+                {
+                    _currentAccumulatedMetrics[metric] = new DoubleAccumulator();
+                }
+                _memoryPool.GetFloatTensor(ref buffer, yExpected.ComputeMetricBufferShape(metric));
+                var metricValue = buffer.ComputeEvaluationMetric(yExpected, yPredicted, metric, _metricData);
+                _currentAccumulatedMetrics[metric].Add(metricValue, yExpected.Shape[0]);
+            }
+            _memoryPool.FreeFloatTensor(buffer);
+        }
+
+        _currentElementCount += yExpected.Shape[0];
+    }
+
+
+    /// <summary>
+    /// true if the metric can only be computed with the full y_true / y_false tensors
+    /// </summary>
+    /// <param name="metric"></param>
+    /// <returns></returns>
+    private static bool RequireFullYToCompute(EvaluationMetricEnum metric)
+    {
+        switch (metric)
+        {
+            case EvaluationMetricEnum.F1Micro:
+            case EvaluationMetricEnum.PearsonCorrelation:
+            case EvaluationMetricEnum.SpearmanCorrelation:
+            case EvaluationMetricEnum.AUC:
+            case EvaluationMetricEnum.AveragePrecisionScore:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+    public List<KeyValuePair<EvaluationMetricEnum, double>> Metrics()
+    {
+        List<KeyValuePair<EvaluationMetricEnum, double>> res = new();
+
+        if (_metricData.Me
