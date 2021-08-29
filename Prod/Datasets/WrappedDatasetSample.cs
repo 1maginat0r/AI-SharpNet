f3c6ebf203fe2aee0f910b4c238787fa03f1334f
1maginat0r <imaginai90@proton.me>
2021-08-29 19:47:52 +0000

    update test.html

--- Prod/Datasets/WrappedDatasetSample.cs
+++ Prod/Datasets/WrappedDatasetSample.cs
@@ -15,4 +15,12 @@ public abstract class WrappedDatasetSample : AbstractDatasetSample
     public override string IdColumn => Original.IdColumn;
     public override string[] TargetLabels => Original.TargetLabels;
     public override Objective_enum GetObjective() => Original.GetObjective();
-    public override DataFrame Predictions_InModelFormat_2_Predictions_InTargetFormat(DataFrame predictions_InModelFormat, Objective_enum objective) => Original.Predictions_InModelFormat_2_Predictions_InTargetFor
+    public override DataFrame Predictions_InModelFormat_2_Predictions_InTargetFormat(DataFrame predictions_InModelFormat, Objective_enum objective) => Original.Predictions_InModelFormat_2_Predictions_InTargetFormat(predictions_InModelFormat, objective);
+    public override int DatasetRows_InModelFormat_MustBeMultipleOf() => Original.DatasetRows_InModelFormat_MustBeMultipleOf();
+    public override char GetSeparator() => Original.GetSeparator();
+    public override void SavePredictions_InTargetFormat(DataFrame y_pred_Encoded_InTargetFormat, DataSet xDataset, string path) => Original.SavePredictions_InTargetFormat(y_pred_Encoded_InTargetFormat, xDataset, path);
+    public override void SavePredictions_InModelFormat(DataFrame predictions_InModelFormat, string path) => Original.SavePredictions_InModelFormat(predictions_InModelFormat, path);
+    public override HashSet<string> FieldsToDiscardInComputeHash() => Original.FieldsToDiscardInComputeHash();
+
+
+}
---
+++ Prod/EvaluationMetricAccumulatorForSingleEpoch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JetBrains.Annotations;
+using SharpNet.Data;
+using SharpNet.Hyperparameters;
+using SharpNet.MathTools;
+
+namespace SharpNet;
+
+public class EvaluationMetricAccumulatorForSingleEpoch : IDisposable
+{
+    private readonly TensorMemoryPool _memoryPool;
+    private readonly int _count;
+    private int _currentElementCount;
+    private readonly IMetricConfig _metricData;
+    private Tensor _full_y_true;
+    private Tensor _full_y_pred;
+    private readonly Dictionary<EvaluationMetricEnum, DoubleAccumulator> _currentAccumulatedMetrics = new();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="memoryPool"></param>
+    /// <param name="count">number of elements (rows) in the dataset</param>
+    /// <param name="metricConfig"></param>
+    public EvaluationMetricAccumulatorForSingleEpoch(TensorMemoryPool memoryPool, int count, IMetricConfig metricConfig)
+    {
+        _memoryPool = memoryPool;
+        _count = count;
+        _metricData = metricConfig;
+    }
+
+    public void UpdateMetrics([NotNull] Tensor yExpected, [NotNull] Tensor yPredicted)
+    {
+        var remainingCount = _count - _currentElementCount;
+        if (remainingCount <= 0) 
+        {
+            throw new ArgumentException($"remainingCount = {remainingCount} and received a tensor of {yExpected.Shape[0]} elements");
+        }
+        if (yExpected.Shape[0] > remainingCount)
+        {
+            var newShape = Utils.CloneShapeWithNewCount(yExpected.Shape, remainingCount);
+            yExpected = yExpected.Reshape(newShape);
+            yPredicted = yPredicted.Reshape(newShape);
+        }
+
+        if 
