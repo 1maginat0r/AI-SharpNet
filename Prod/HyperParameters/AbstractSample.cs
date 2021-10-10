cf06c7de76eba4287cf5e1c422e52ed22b21b942
1maginat0r <imaginai90@proton.me>
2021-10-10 13:30:33 +0000

    Minor update to README

--- Prod/HyperParameters/AbstractSample.cs
+++ Prod/HyperParameters/AbstractSample.cs
@@ -79,4 +79,62 @@ public abstract class AbstractSample : ISample
     {
         return ClassFieldSetter.GetFieldType(GetType(), HyperparameterName);
     }
-    public virtual void Set(IDictionary<
+    public virtual void Set(IDictionary<string, object> dico)
+    {
+        foreach (var (key, value) in dico)
+        {
+            Set(key, value);
+        }
+    }
+    //TODO add tests
+    /// <summary>
+    /// to determine if an Hyper-Parameter is categorical, we'll use the following rules:
+    ///     1/ if the Hyper-Parameter name is in '_mandatoryCategoricalHyperparameters'
+    ///             => it is categorical
+    ///     2/ if the Hyper-Parameter is a int/float/double
+    ///             => it is numerical
+    ///     3/ if the Hyper-Parameter is a bool/string/enum
+    ///             => it is categorical
+    ///     4/ in all other cases
+    ///             => an exception is thrown (can't determine if the Hyper-Parameter is categorical)
+    /// </summary>
+    /// <param name="HyperparameterName"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public virtual bool IsCategoricalHyperparameter(string HyperparameterName)
+    {
+        if (_mandatoryCategoricalHyperparameters.Contains(HyperparameterName))
+        {
+            return true;
+        }
+        var HyperparameterType = GetFieldType(HyperparameterName);
+        if (HyperparameterType == typeof(double) || HyperparameterType == typeof(float) || HyperparameterType == typeof(int))
+        {
+            return false;
+        }
+        if (HyperparameterType == typeof(string) || HyperparameterType == typeof(bool) || HyperparameterType.IsEnum || HyperparameterType.IsClass)
+        {
+            return true;       
+        }
+        throw new ArgumentException($"can't determine if {HyperparameterName} ({HyperparameterType}) field of class {GetType()} is categorical");
+    }
+    #endregion
+
+    public virtual HashSet<string> FieldsToDiscardInComputeHash()
+    {
+        return new HashSet<string>();
+    }
+
+    protected virtual string ToConfigContent(Func<string, object, bool> accept)
+    {
+        var result = new List<string>();
+        foreach (var (parameterName,fieldValue) in ToDictionaryConfigContent(accept).OrderBy(f => f.Key))
+        {
+            var fieldValueToString = Utils.FieldValueToString(fieldValue);
+            result.Add($"{parameterName} = {fieldValueToString}");
+        }
+        return string.Join(Environment.NewLine, result) + Environment.NewLine;
+    }
+    protected IDictionary<string,object> ToDictionaryConfigContent(Func<string, object, bool> accept)
+    {
+        var result = n
