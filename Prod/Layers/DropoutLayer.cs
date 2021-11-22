9892e263f4d2479a1b5919c3b13585c04f150567
1maginat0r <imaginai90@proton.me>
2021-11-22 05:32:15 +0000

    Ugh.

--- Prod/Layers/DropoutLayer.cs
+++ Prod/Layers/DropoutLayer.cs
@@ -34,4 +34,58 @@ namespace SharpNet.Layers
                 //no need of dropout reserved space for inference
                 FreeFloatTensor(ref _dropoutReservedSpaceForTraining);
             }
-            x.Dr
+            x.DropoutForward(y, _dropoutRate, isTraining, Rand, _dropoutReservedSpaceForTraining);
+            if (!BackwardPropagationNeeded(isTraining, FirstTrainableLayer(Layers)))
+            {
+                FreeFloatTensor(ref _dropoutReservedSpaceForTraining);
+            }
+        }
+
+        public override void BackwardPropagation(List<Tensor> allX, Tensor y_NotUsed, Tensor dy, List<Tensor> dx)
+        {
+            Debug.Assert(allX.Count == 1);
+            Debug.Assert(y_NotUsed == null);
+            Debug.Assert(dx.Count == 1);
+            Debug.Assert(_dropoutReservedSpaceForTraining != null);
+            allX[0].DropoutBackward(dy, dx[0], _dropoutRate, _dropoutReservedSpaceForTraining);
+            FreeFloatTensor(ref _dropoutReservedSpaceForTraining);
+        }
+        public override bool OutputNeededForBackwardPropagation => false;
+        #endregion
+
+        #region serialization
+        public override string Serialize()
+        {
+            return RootSerializer().Add(nameof(_dropoutRate), _dropoutRate).ToString();
+        }
+        public static DropoutLayer Deserialize(IDictionary<string, object> serialized, Network network)
+        {
+            var dropoutRate = serialized.ContainsKey("_dropProbability") 
+                    ? (double)serialized["_dropProbability"]
+                    :(double)serialized[nameof(_dropoutRate)];
+            return new DropoutLayer(dropoutRate, network, (string)serialized[nameof(LayerName)]);
+        }
+        public override void AddToOtherNetwork(Network otherNetwork) { AddToOtherNetwork(otherNetwork, Deserialize); }
+        #endregion
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            //managed memory
+            FreeFloatTensor(ref _dropoutReservedSpaceForTraining);
+        }
+
+        private void InitializeDropoutReservedSpaceForTraining(Tensor x)
+        {
+            if (x.UseGPU)
+            {
+                var xDesc = Network.GpuWrapper.TensorDesc(cudnnDataType_t.CUDNN_DATA_FLOAT, x.Shape);
+                var res = CudnnWrapper.cudnnDropoutGetReserveSpaceSize(xDesc, out var dropoutReservedSpaceInBytes);
+                GPUWrapper.CheckStatus(res);
+                MemoryPool.GetBuffer(ref _dropoutReservedSpaceForTraining, dropoutReservedSpaceInBytes);
+            }
+            else
+            {
+                GetFloatTensor(ref _dropoutReservedSpaceForTraining, x.Shape);
+            }
+            Debug.Assert(_dropoutReservedSpaceForTraining.UseGPU == x.UseG
