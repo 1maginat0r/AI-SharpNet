2199419671e9ba7a7c95b7836aa39791e24efa88
1maginat0r <imaginai90@proton.me>
2021-12-23 14:07:10 +0000

    New translations messages.json (Chinese Traditional)

--- Prod/Layers/FlattenLayer.cs
+++ Prod/Layers/FlattenLayer.cs
@@ -1,3 +1,67 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
-using S
+using SharpNet.Data;
+using SharpNet.Networks;
+
+namespace SharpNet.Layers;
+
+public class FlattenLayer : Layer
+{
+    private readonly bool _flattenInputTensorOnLastDimension;
+
+    /// <summary>
+    /// convert the input tensor to a 2D Tensor
+    /// if input shape is:
+    ///     (batchSize, a,b, ...., y,z)
+    /// output shape will be:
+    ///     (batchSize, a*b*y*z)                    if flattenInputTensorOnLastDimension == false
+    ///     (batchSize*a*b*y, z)                    if flattenInputTensorOnLastDimension == true
+    /// </summary>
+    /// <param name="flattenInputTensorOnLastDimension"></param>
+    /// <param name="network"></param>
+    /// <param name="layerName"></param>
+    public FlattenLayer(bool flattenInputTensorOnLastDimension, Network network, string layerName = "") : base(network, layerName)
+    {
+        _flattenInputTensorOnLastDimension = flattenInputTensorOnLastDimension;
+    }
+
+    #region forward and backward propagation
+    public override void ForwardPropagation(List<Tensor> allX, Tensor y, bool isTraining)
+    {
+        Debug.Assert(allX.Count == 1);
+        allX[0].CopyTo(y);
+    }
+    public override void BackwardPropagation(List<Tensor> allX_NotUsed, Tensor y_NotUsed, Tensor dy, List<Tensor> dx)
+    {
+        Debug.Assert(allX_NotUsed.Count == 0);
+        Debug.Assert(y_NotUsed == null);
+        Debug.Assert(dx.Count == 1);
+        if (PrevLayer.IsInputLayer)
+        {
+            return;
+        }
+        dy.CopyTo(dx[0]);
+    }
+    public override bool OutputNeededForBackwardPropagation => false;
+    public override bool InputNeededForBackwardPropagation => false;
+
+    #endregion
+
+    #region serialization
+    public override string Serialize()
+    {
+        return RootSerializer()
+            .Add(nameof(_flattenInputTensorOnLastDimension), _flattenInputTensorOnLastDimension)
+            .ToString();
+    }
+    public static FlattenLayer Deserialize(IDictionary<string, object> serialized, Network network)
+    {
+        var flattenInputTensorOnLastDimension = serialized.GetOrDefault(nameof(_flattenInputTensorOnLastDimension), false);
+        return new FlattenLayer(flattenInputTensorOnLastDimension, network);
+    }
+    public override void AddToOtherNetwork(Network otherNetwork) { AddToOtherNetwork(otherNetwork, Deserialize); }
+    #endregion
+
+
+    #region PyTorch support
+    public override void ToPytorchModule(List<string> constructorLines, List<string> forwardL
