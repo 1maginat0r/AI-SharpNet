dc9ae668107a62a5a0dbc7a4cbdfce8ab302495d
1maginat0r <imaginai90@proton.me>
2022-06-11 12:06:29 +0000

    Updated REAMDE.md

--- Prod/LightGBM/LightGBMModel.cs
+++ Prod/LightGBM/LightGBMModel.cs
@@ -6,4 +6,51 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using SharpNet.Datasets;
-using SharpNet.Model
+using SharpNet.Models;
+
+namespace SharpNet.LightGBM
+{
+    public class LightGBMModel : Model
+    {
+        #region public fields & properties
+        public LightGBMSample LightGbmSample => (LightGBMSample)ModelSample;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lightGbmModelSample"></param>
+        /// <param name="workingDirectory"></param>
+        /// <param name="modelName">the name of the model to use</param>
+        /// <exception cref="Exception"></exception>
+        [SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
+        public LightGBMModel(LightGBMSample lightGbmModelSample, string workingDirectory, [JetBrains.Annotations.NotNull] string modelName): base(lightGbmModelSample, workingDirectory, modelName)
+        {
+            if (!File.Exists(ExePath))
+            {
+                throw new Exception($"Missing exe {ExePath}");
+            }
+            if (!Directory.Exists(TempPath))
+            {
+                Directory.CreateDirectory(TempPath);
+            }
+        }
+        #endregion
+
+        public override (string train_XDatasetPath_InModelFormat, string train_YDatasetPath_InModelFormat, string train_XYDatasetPath_InModelFormat, string validation_XDatasetPath_InModelFormat, string validation_YDatasetPath_InModelFormat, string validation_XYDatasetPath_InModelFormat, IScore trainLossIfAvailable, IScore validationLossIfAvailable, IScore trainRankingMetricIfAvailable, IScore validationRankingMetricIfAvailable)
+            Fit(DataSet trainDataset, DataSet validationDatasetIfAny, Func<bool, bool, DataSet, DataSet, string, List<string>> save = null)
+        {
+            if (ModelSample.GetLoss()  == EvaluationMetricEnum.DEFAULT_VALUE)
+            {
+                throw new ArgumentException("Loss Function not set");
+            }
+
+            var sw = Stopwatch.StartNew();
+            const bool addTargetColumnAsFirstColumn = true;
+            const bool includeIdColumns = false;
+            const bool overwriteIfExists = false;
+            var train_XYDatasetPath_InModelFormat = trainDataset.to_csv_in_directory(DatasetPath, addTargetColumnAsFirstColumn, includeIdColumns, overwriteIfExists);
+            var validation_XYDatasetPath_InModelFormat = validationDatasetIfAny?.to_csv_in_directory(DatasetPath, addTargetColumnAsFirstColumn, includeIdColumns, overwriteIfExists);
+            LightGbmSample.UpdateForDataset(trainDataset);
+            //we s
