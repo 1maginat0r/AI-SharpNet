4ce57b9277388ecff47d0c56864ca719167fbea5
1maginat0r <imaginai90@proton.me>
2022-06-19 09:14:44 +0000

    p54usb: add USB ID for Corega WLUSB2GTST USB adapter

--- Prod/LightGBM/LightGBMSample.cs
+++ Prod/LightGBM/LightGBMSample.cs
@@ -27,4 +27,66 @@ public class LightGBMSample : AbstractModelSample
             case objective_enum.DEFAULT_VALUE:
                 return EvaluationMetricEnum.DEFAULT_VALUE;
             case objective_enum.regression:
-                return EvaluationMetricEnum
+                return EvaluationMetricEnum.Rmse;
+            case objective_enum.regression_l1:
+                return EvaluationMetricEnum.Mae;
+            case objective_enum.binary:
+                return EvaluationMetricEnum.BinaryCrossentropy;
+            case objective_enum.multiclass:
+            case objective_enum.cross_entropy: //TODO to check
+                return EvaluationMetricEnum.CategoricalCrossentropy; 
+            default:
+                throw new NotImplementedException($"can't manage metric {objective}");
+        }
+    }
+
+    public override EvaluationMetricEnum GetRankingEvaluationMetric()
+    {
+        return GetAllEvaluationMetrics()[0];
+    }
+    public static EvaluationMetricEnum ToEvaluationMetricEnum(string metric)
+    {
+        if (string.IsNullOrEmpty(metric))
+        {
+            return EvaluationMetricEnum.DEFAULT_VALUE;
+        }
+        switch (metric.ToLowerInvariant())
+        {
+            case "rmse":
+            case "regression":
+                return EvaluationMetricEnum.Rmse;
+            case "regression_l1":
+            case "mae":
+                return EvaluationMetricEnum.Mae;
+            case "huber":
+                return EvaluationMetricEnum.Huber;
+            case "auc":
+                return EvaluationMetricEnum.AUC;
+            case "average_precision":
+                return EvaluationMetricEnum.AveragePrecisionScore;
+            case "binary":
+                return EvaluationMetricEnum.BinaryCrossentropy;
+            case "multiclass":
+            case "cross_entropy":
+                return EvaluationMetricEnum.CategoricalCrossentropy;
+            case "accuracy":
+                return EvaluationMetricEnum.Accuracy;
+            default:
+                throw new NotImplementedException($"can't manage metric {metric}");
+        }
+    }
+
+    public static string ToStringMetric(EvaluationMetricEnum metric)
+    {
+        switch (metric)
+        {
+            case EvaluationMetricEnum.DEFAULT_VALUE: return "";
+            case EvaluationMetricEnum.Rmse: return "rmse";
+            case EvaluationMetricEnum.Mae:  return "mae";
+            case EvaluationMetricEnum.Huber : return "huber";
+            case EvaluationMetricEnum.AUC: return  "auc";
+            case EvaluationMetricEnum.AveragePrecisionScore: return "average_precision";
+            case EvaluationMetricEnum.BinaryCrossentropy: return  "binary" ;
+            case EvaluationMetricEnum.CategoricalCrossentropy: return "cross_entropy";
+            case EvaluationMetricEnum.Accuracy: return  "accuracy";
+      
