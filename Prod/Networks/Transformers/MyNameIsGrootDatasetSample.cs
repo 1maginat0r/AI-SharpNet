62db45ed080500bdbac77a5468cca553c16456d0
1maginat0r <imaginai90@proton.me>
2022-09-18 15:23:14 +0000

    net: qmi_wwan: new devices: UML290 and K5006-Z

--- Prod/Networks/Transformers/MyNameIsGrootDatasetSample.cs
+++ Prod/Networks/Transformers/MyNameIsGrootDatasetSample.cs
@@ -28,4 +28,49 @@ public class MyNameIsGrootDatasetSample : TransformerDatasetSample
 
     private const string sentence = "my name is groot";
 
-    public MyNameIsG
+    public MyNameIsGrootDatasetSample()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < 20_000; ++i)
+        //for (int i = 0; i < 2; ++i)
+        {
+            sb.Append(sentence + " ");
+        }
+        _fullText = sb.ToString();
+    }
+
+    public Tokenizer GetTokenizer()
+    {
+        var tokenizer = new Tokenizer(vocab_size, oovToken: null, char_level: false, lowerCase: true, firstElementIsPaddingToken: false);
+        tokenizer.FitOnTexts(new[] { _fullText });
+        return tokenizer;
+    }
+
+    public override DataFrame Predictions_InModelFormat_2_Predictions_InTargetFormat(DataFrame predictions_InModelFormat, Objective_enum objective)
+    {
+        return predictions_InModelFormat;
+    }
+
+    public override string IdColumn => null;
+    public override string[] TargetLabels { get; } = {"y"};
+    public override bool IsCategoricalColumn(string columnName) => DefaultIsCategoricalColumn(columnName);
+    public override Objective_enum GetObjective()
+    {
+        return Objective_enum.Classification;
+    }
+    public override DataSet TestDataset()
+    {
+        return null;
+    }
+
+    public override DataSet FullTrainingAndValidation()
+    {
+        return new TransformerDataset(
+            this,
+            MyNameIsGrootUtils.NAME,
+            _fullText,
+            GetTokenizer());
+    }
+
+    public string GetText() => _fullText;
+}
---
+++ Prod/Networks/Transformers/MyNameIsGrootUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using log4net;
+using SharpNet.CPU;
+using SharpNet.Datasets;
+using SharpNet.HPO;
+using SharpNet.Hyperparameters;
+// ReSharper disable UnusedMember.Global
+
+namespace SharpNet.Networks.Transformers;
+
+public static class MyNameIsGrootUtils
+{
+    public const string NAME = "MyNameIsGroot";
+
+
+    #region public fields & properties
+    private static readonly ILog Log = LogManager.GetLogger(typeof(MyNameIsGrootUtils));
+    #endregion
+
+    public static string WorkingDirectory => Path.Combine(Utils.ChallengesPath, NAME);
+
+
+    private static string GenerateText(Network nn, int textLength, double maxAllowedError)
+    {
+        var outputShape = nn.YPredicted_MiniBatch_Shape(1);
+        var max_length = outputShape[1];
+        var vocab_size = outputShape[2];
+        var datasetSample = new MyNameIsGrootDatasetSample() { max_length = max_length, vocab_size = vocab_size };
+        var tokenizer = datasetSample.GetTokenizer();
+
+        var xInputSingleRow = new CpuTensor<float>
