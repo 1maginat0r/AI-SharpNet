8a082b0be87055581d334a3f934f0a72a426f949
1maginat0r <imaginai90@proton.me>
2022-09-18 09:41:35 +0000

    parport: parport_pc: remove double PCI ID for NetMos

--- Prod/Networks/Transformers/CharLevelTransformersDatasetSample.cs
+++ Prod/Networks/Transformers/CharLevelTransformersDatasetSample.cs
@@ -34,4 +34,54 @@ public class CharLevelTransformersDatasetSample : TransformerDatasetSample
     static CharLevelTransformersDatasetSample()
     {
         Utils.ConfigureGlobalLog4netProperties(TextTransformersUtils.WorkingDirectory, "log");
-        Utils.ConfigureThreadLog4netProperties(TextTransformersUtils.WorkingDi
+        Utils.ConfigureThreadLog4netProperties(TextTransformersUtils.WorkingDirectory, "log");
+    }
+
+    public CharLevelTransformersDatasetSample()
+    {
+        //_fullText = File.ReadAllText(TextTransformersUtils.XTrainPath).Substring(0, 3_000); //!D;
+        _fullText = File.ReadAllText(TextTransformersUtils.XTrainPath);
+    }
+
+    public Tokenizer GetTokenizer()
+    {
+        var tokenizer = new Tokenizer(vocab_size, oovToken: null, char_level: true, lowerCase: false, filters: "\r",
+            firstElementIsPaddingToken: false);
+        tokenizer.FitOnTexts(new[] { _fullText });
+        return tokenizer;
+    }
+
+    public string GetText(int maxCharacters = -1)
+    {
+        if (maxCharacters != -1 && maxCharacters < _fullText.Length)
+        {
+            return _fullText.Substring(0, maxCharacters);
+        }
+        return _fullText;
+    }
+
+    public override DataFrame Predictions_InModelFormat_2_Predictions_InTargetFormat(DataFrame predictions_InModelFormat, Objective_enum objective)
+    {
+        return predictions_InModelFormat;
+    }
+    public override string IdColumn => null;
+    public override string[] TargetLabels { get; } = null;
+    public override bool IsCategoricalColumn(string columnName) => DefaultIsCategoricalColumn(columnName);
+    public override Objective_enum GetObjective()
+    {
+        return Objective_enum.Classification;
+    }
+    public override DataSet TestDataset()
+    {
+        return null;
+    }
+
+    public override DataSet FullTrainingAndValidation()
+    {
+        return new TransformerDataset(
+            this,
+            "CharLevel",
+            GetText(MaxCharacterLengthForTraining),
+            GetTokenizer());
+    }
+}
---
+++ Prod/Networks/Transformers/MyNameIsGrootDatasetSample.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using log4net;
+using SharpNet.Datasets;
+using SharpNet.TextPreprocessing;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace SharpNet.Networks.Transformers;
+
+public class MyNameIsGrootDatasetSample : TransformerDatasetSample
+{
+    #region private static fields
+
+    private readonly string _fullText;
+
+    #endregion
+
+    #region public fields & properties
+    // ReSharper disable once UnusedMember.Global
+    public static readonly ILog Log = LogManager.GetLogger(typeof(MyNameIsGrootDatasetSample));
+    #endregion
+
+
+    static MyNameIsGrootDatasetSample()
+    {
+        Utils.ConfigureGlobalLog4netProperties(MyNameIsGrootUtils.WorkingDirectory, "log");
+        Utils.ConfigureThreadLog4netProperties(MyNameIsGrootUtils.WorkingDirectory, "log");
+    }
+
+    private const string sentence = "my name is groot";
+
+    public MyNameIsG
