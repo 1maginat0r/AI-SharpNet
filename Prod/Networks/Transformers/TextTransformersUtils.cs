4d22f6c2eb0c82af1d636c5ce10a283c0da0fcc5
1maginat0r <imaginai90@proton.me>
2022-10-01 22:50:41 +0000

    Modified.

--- Prod/Networks/Transformers/TextTransformersUtils.cs
+++ Prod/Networks/Transformers/TextTransformersUtils.cs
@@ -17,4 +17,59 @@ public static class TextTransformersUtils
 
 
     #region public fields & properties
-    private static readonly ILog Log = LogManager.GetLogger(typeof(Tex
+    private static readonly ILog Log = LogManager.GetLogger(typeof(TextTransformersUtils));
+    #endregion
+
+    public static string WorkingDirectory => Path.Combine(Utils.ChallengesPath, NAME);
+    private static string DataDirectory => Path.Combine(WorkingDirectory, "Data");
+    // ReSharper disable once MemberCanBePrivate.Global
+
+    //public static string XTrainPath => Path.Combine(DataDirectory, "input_gpt_dev.txt");
+    public static string XTrainPath => Path.Combine(DataDirectory, "victor_hugo_v1.txt");
+
+    private static string GenerateText(Network nn, int textLength, double maxAllowedError)
+    {
+        var outputShape = nn.YPredicted_MiniBatch_Shape(1);
+        var max_length = outputShape[1];
+        var vocab_size = outputShape[2];
+        var datasetSample = new CharLevelTransformersDatasetSample() { max_length = max_length, vocab_size = vocab_size };
+        var tokenizer = datasetSample.GetTokenizer();
+
+        var xInputSingleRow = new CpuTensor<float>(new[] { 1, max_length});
+        var xInputSingleRowSpan = xInputSingleRow.SpanContent;
+
+        var fulltext = datasetSample.GetText(-1);
+        var r = new Random();
+        int randomStartIdx = r.Next(fulltext.Length/2 - max_length-1);
+
+        List<int> tmpSequence = tokenizer.TextsToSequences(new[] { fulltext.Substring(randomStartIdx, 10*max_length) })[0].Take(max_length).ToList();
+
+        int[] newSequence = new int[max_length+textLength];
+        for (int j = 0; j < max_length; j++)
+        {
+            newSequence[j] = tmpSequence[j];
+        }
+
+        int nextIndexToGenerate = max_length;
+        while (nextIndexToGenerate < newSequence.Length)
+        {
+            //we know the sequence newSequence[nextIndexToGenerate-max_length] to newSequence[nextIndexToGenerate-1]
+            //we want to compute next sequence item at position newSequence[nextIndexToGenerate]
+
+            for (int i = 0; i < max_length; i++)
+            {
+                xInputSingleRowSpan[i] = newSequence[nextIndexToGenerate - max_length+i];
+            }
+
+            var prediction = nn.Predict(xInputSingleRow, false);
+            var proba = prediction.As2DTensor(true).RowSlice(max_length-1, 1).ContentAsFloatArray();
+            var indexNextToken = GetIndexPrediction(proba, r, maxAllowedError);
+            newSequence[nextIndexToGenerate] = indexNextToken;
+            ++nextIndexToGenerate;
+        }
+        var generateText  =tokenizer.SequenceToText(newSequence.Skip(max_length));
+        return generateText;
+    }
+
+
+    private stati
