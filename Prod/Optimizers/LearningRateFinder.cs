ab9a2ecbdd2f5fac5dde1217d568a27680287695
1maginat0r <imaginai90@proton.me>
2022-10-23 16:49:26 +0000

    Newline at end of file

--- Prod/Optimizers/LearningRateFinder.cs
+++ Prod/Optimizers/LearningRateFinder.cs
@@ -27,4 +27,54 @@ namespace SharpNet.Optimizers
 
         #endregion
 
-        public LearningRateFinder(int miniBatchSize, int entireBatchSize, doub
+        public LearningRateFinder(int miniBatchSize, int entireBatchSize, double minLearningRate, double maxLearningRate, double momentum = 0.95)
+        {
+            _momentum = momentum;
+            _minLearningRate = minLearningRate;
+            MaxLearningRate = maxLearningRate;
+            _nbBlocksPerEpoch = (entireBatchSize + miniBatchSize - 1) / miniBatchSize;
+            _multiplicativeCoeff = Math.Pow(maxLearningRate / minLearningRate, (1.0 / (_nbBlocksPerEpoch - 1)));
+        }
+
+        public double LearningRate(int epoch, double percentagePerformedInEpoch, double learningRateMultiplicativeFactorFromReduceLrOnPlateau)
+        {
+            Debug.Assert(epoch >= 1);
+            Debug.Assert(percentagePerformedInEpoch >= 0);
+            Debug.Assert(percentagePerformedInEpoch <= 1);
+            var currentStep = _nbBlocksPerEpoch * percentagePerformedInEpoch;
+            return _minLearningRate * Math.Pow(_multiplicativeCoeff, currentStep);
+        }
+
+        /// <summary>
+        /// at the end of each block id, this method is called with the loss computed in the last block
+        /// </summary>
+        /// <param name="loss">loss associated with the last computed block</param>
+        /// <returns>true if we should stop the computation (no need to look to other blocks)
+        /// false if we should continue</returns>
+        public bool AddLossForLastBlockId(double loss)
+        {
+            if (double.IsNaN(loss) ||double.IsInfinity(loss))
+            {
+                return true;
+            }
+            _loss.Add(loss);
+            var prevAvgLoss = (_avgLosses.Count == 0 ? 0 : _avgLosses.Last());
+            var avgLoss = _momentum * prevAvgLoss + (1 - _momentum) * loss;
+            _avgLosses.Add(avgLoss);
+            var smoothedLoss = avgLoss / (1.0-Math.Pow(_momentum, 1+_smoothedLosses.Count));
+            _smoothedLosses.Add(smoothedLoss);
+            return false;
+        }
+        public double BestLearningRate()
+        {
+            if (_multiplicativeCoeff <= 1.0)
+            {
+                return 0.0;
+            }
+            var nbBlocksBetweenAFactor10InLearningRate = (int)Math.Ceiling(Math.Log(10) / Math.Log(_multiplicativeCoeff));
+            double maxDecreaseInLoss = double.MinValue;
+            double bestLearningRate = double.NaN;
+            for (int blockId = 10 + nbBlocksBetweenAFactor10InLearningRate;blockId < _smoothedLosses.Count - 5;++blockId)
+            {
+                var learningRateForBatchBlock = LearningRate(1, blockId/(double)_nbBlocksPerEpoch, 1.0);
+                //we measure the redu
