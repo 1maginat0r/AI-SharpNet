ea06abec77fabd3bc1de3c5b65a6c00d645fa9be
1maginat0r <imaginai90@proton.me>
2022-11-02 08:44:03 +0000

    added travis

--- Prod/Optimizers/LearningRateScheduler.cs
+++ Prod/Optimizers/LearningRateScheduler.cs
@@ -17,4 +17,50 @@ namespace SharpNet.Optimizers
         /// else:
         ///     use a linear learning rate from the start to the end of the epoch
         /// </summary>
-        private readonly bool ConstantInEa
+        private readonly bool ConstantInEachEpoch;
+
+        #endregion
+
+        public bool ShouldCreateSnapshotForEpoch(int epoch) { return false; }
+
+        #region Constructors
+        private LearningRateScheduler(List<Tuple<double, double>> values, bool constantByInterval, bool constantInEachEpoch)
+        {
+            Debug.Assert(values != null);
+            Debug.Assert(values.Count >= 1);
+            Values = values;
+            _constantByInterval = constantByInterval;
+            ConstantInEachEpoch = constantInEachEpoch;
+            MaxLearningRate = Values.Select(v => v.Item2).Max();
+        }
+
+        public double MaxLearningRate { get; }
+
+        public static LearningRateScheduler Constant(double learningRate)
+        {
+            var values = new List<Tuple<double, double>> { Tuple.Create(1.0, learningRate) };
+            return new LearningRateScheduler(values, false, true);
+        }
+        public static LearningRateScheduler ConstantByInterval(int epoch1, double learningRate1, int epoch2, double learningRate2)
+        {
+            return ByInterval(epoch1, learningRate1, epoch2, learningRate2, true, true);
+        }
+        public static LearningRateScheduler ConstantByInterval(int epoch1, double learningRate1, int epoch2, double learningRate2, int epoch3, double learningRate3)
+        {
+            return ByInterval(epoch1, learningRate1, epoch2, learningRate2, epoch3, learningRate3, true);
+        }
+        public static LearningRateScheduler ConstantByInterval(int epoch1, double learningRate1, int epoch2, double learningRate2, int epoch3, double learningRate3, int epoch4, double learningRate4)
+        {
+            return ByInterval(epoch1, learningRate1, epoch2, learningRate2, epoch3, learningRate3, epoch4, learningRate4, true);
+        }
+        public static LearningRateScheduler InterpolateByInterval(int epoch1, double learningRate1, int epoch2, double learningRate2)
+        {
+            return ByInterval(epoch1, learningRate1, epoch2, learningRate2, false, true);
+        }
+        public static LearningRateScheduler Linear(double initialLearningRate, int lastEpoch, double lastLearningRate)
+        {
+            return ByInterval(0, initialLearningRate, lastEpoch, lastLearningRate, false, false);
+        }
+        public static LearningRateScheduler InterpolateByInterval(int epoch1, double learningRate1, int epoch2, double learningRate2, int epoch3, double learningRate3)
+        {
+            return ByInterval(epoch1, learningRate1, epoch2, learningRate2, epoch3,
