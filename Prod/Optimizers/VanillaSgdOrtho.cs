8770d4cd4466665452df81742f5f295c218d6e0b
1maginat0r <imaginai90@proton.me>
2022-12-31 12:39:20 +0000

    wiki changes

--- Prod/Optimizers/VanillaSgdOrtho.cs
+++ Prod/Optimizers/VanillaSgdOrtho.cs
@@ -57,4 +57,69 @@ public class VanillaSgdOrtho : Optimizer
     {
         Debug.Assert(weights.SameShape(weightGradients));
         Debug.Assert(bias == null || bias.SameShape(biasGradients));
-        var ponderedLearningRate = PonderedLearnin
+        var ponderedLearningRate = PonderedLearning(learningRate, batchSize);
+        /*
+          W_new = 
+            W 
+            - 
+            group['lr'].cuda()
+            *
+            (
+               torch.mm(													(m,n)
+                        tmp_I - torch.mm(W, W.t()) ,						(m, m)
+                        dW													(m, n)
+                       ) 
+             + 0.5*torch.mm(												(m,n)
+                        W, 													(m, n)
+                        torch.mm(W.t(), dW) - torch.mm(dW.t(), W)			(n,n)
+                       )
+            )
+        */
+
+        var buffer_mn = Q;
+        var buffer_nn = R;
+
+        buffer_mm.SetIdentityMatrix();
+        buffer_mm.Dot(weights, false, weights, true, -1.0f, 1.0f); // tmp_I - torch.mm(W, W.t())
+        buffer_mn.Dot(buffer_mm, weightGradients); // torch.mm( tmp_I - torch.mm(W, W.t()) , dW )
+
+        buffer_nn.Dot(weights, true, weightGradients, false, 1.0f, 0.0f); // torch.mm(W.t(), dW)
+        buffer_nn.Dot(weightGradients, true, weights, false, -1.0f, 1.0f); //torch.mm(W.t(), dW) - torch.mm(dW.t(), W)
+        buffer_mn.Dot(weights, false, buffer_nn, false, 0.5f, 1.0f); // torch.mm( tmp_I - torch.mm(W, W.t()) , dW )+ 0.5*torch.mm( W, torch.mm( tmp_I - torch.mm(W, W.t()) , dW ) )
+
+        weights.Update_Adding_Alpha_X(-ponderedLearningRate, buffer_mn);
+        weights.QRFactorization(Q,R, QRFactorization_buffer);
+        //!D TODO update weights to make it unique
+        Q.CopyTo(weights);
+
+        /*
+          W_new = W - group['lr'].cuda()*(torch.mm((tmp_I - torch.mm(W, W.t())),dW) + 0.5*torch.mm(W, (torch.mm(W.t(), dW) - torch.mm(dW.t(), W))))
+                            Q, R = torch.qr(W_new)
+                            D = torch.diag(torch.sign(torch.diag(R)))
+                            Q = torch.mm(Q,D)
+
+                            if n_feature_in <= n_feature_out:
+                                p.data = Q
+         */
+
+
+        bias?.Update_Adding_Alpha_X(-ponderedLearningRate, biasGradients);
+    }
+    public override void Dispose()
+    {
+        if (_isDisposed)
+        {
+            return;
+        }
+        _isDisposed = true;
+        base.Dispose();
+        EmbeddedTensors.ForEach(t => _memoryPool?.FreeFloatTensor(t));
+    }
+
+    #region serialization
+    public override string Serialize()
+    {
+        return new Serializer()
+            .Add(nameof(Q), Q)
+            .Add(nameof(R), R)
+            .Add(nameof(QRFactorization_buffer), QRFactori
