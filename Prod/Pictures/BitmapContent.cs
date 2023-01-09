bbb9fbc2fc5860a0c9591e50630c0d39c14a94e9
1maginat0r <imaginai90@proton.me>
2023-01-09 09:26:30 +0000

    update types

--- Prod/Pictures/BitmapContent.cs
+++ Prod/Pictures/BitmapContent.cs
@@ -47,4 +47,57 @@ namespace SharpNet.Pictures
             int heightAndWidth = alwaysCropInsidePicture
                 ? Math.Min(GetHeight(), GetWidth())
                 : Math.Max(GetHeight(), GetWidth());
-     
+            var content = new byte[3 * heightAndWidth * heightAndWidth];
+            var result = new BitmapContent(new[] { Shape[0], heightAndWidth, heightAndWidth }, content);
+            bool swapWidthAndHeight = alwaysUseBiggestSideForWidthSide && GetHeight() > GetWidth();
+            
+            for (int channel = 0; channel < Shape[0]; ++channel)
+            {
+                var filling = (channel == 0) ? fillingColor.Item1 : (channel == 1 ? fillingColor.Item2 : fillingColor.Item3);
+                for (int row = 0; row < heightAndWidth; ++row)
+                {
+                    int originalRow = row + (GetHeight()-heightAndWidth) / 2;
+                    for (int col = 0; col < heightAndWidth; ++col)
+                    {
+                        int originalCol = col + (GetWidth() - heightAndWidth) / 2;
+                        var originalVal = (originalRow >= 0 && originalCol >= 0 && originalRow < GetHeight() && originalCol < GetWidth())
+                            ? Get(channel, originalRow, originalCol)
+                            : filling;
+                        if (swapWidthAndHeight)
+                        {
+                            result.Set(channel, col, row, originalVal);
+                        }
+                        else
+                        {
+                            result.Set(channel, row, col, originalVal);
+                        }
+                    }
+                }
+            }
+            return result;
+
+        }
+        /// <summary>
+        /// return the SHA-1 of a bitmap (160 bits stored in a string of 40 bytes in hexadecimal format: 0=>f)
+        /// </summary>
+        public string SHA1()
+        {
+            var byteArray = Content.ToArray();
+            using var sha1 = new SHA1CryptoServiceProvider();
+            var hashBytes = sha1.ComputeHash(byteArray);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+        public void Save(params string[] fileNames)
+        {
+            if (fileNames.Length== 1)
+            {
+                Debug.Assert(GetChannels() == 3);
+                var bmp = AsBitmap();
+                PictureTools.SavePng(bmp, fileNames[0]);
+                bmp.Dispose();
+            }
+            else
+            {
+                Debug.Assert(GetChannels() == fileNames.Length);
+                for (int channel = 0; channel < GetChannels(); ++channel)
+                {
