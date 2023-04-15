4eb6f0ec31545490e476498a13c3238027983c3a
1maginat0r <imaginai90@proton.me>
2023-04-15 18:52:51 +0000

    code coverage

--- Tests/DataAugmentation/Operations/FlipTests.cs
+++ Tests/DataAugmentation/Operations/FlipTests.cs
@@ -75,4 +75,48 @@ namespace SharpNetTests.DataAugmentation.Operations
             var expected = new[] { 12f };
             OperationTests.CheckAllPermutations(new VerticalFlip(inputShape[2]), new HorizontalFlip(inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
 
-            //sing
+            //single line
+            input = new[] { 12f, 13, 14 };
+            inputShape = new[] { 1, 1, 1, 3 };
+            expected = new[] { 14f, 13, 12 };
+            OperationTests.CheckAllPermutations(new VerticalFlip(inputShape[2]), new HorizontalFlip(inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
+
+            //single column
+            input = new[] { 12f, 13, 14 };
+            inputShape = new[] { 1, 1, 3, 1 };
+            expected = new[] { 14f, 13, 12 };
+            OperationTests.CheckAllPermutations(new VerticalFlip(inputShape[2]), new HorizontalFlip(inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
+
+            // 4x4 matrix
+            input = Enumerable.Range(0, 16).Select(x => (float)x).ToArray();
+            inputShape = new[] { 1, 1, 4, 4 };
+            expected = new[] { 15f, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            var verticalFlip = new VerticalFlip(inputShape[2]);
+            var horizontalFlip = new HorizontalFlip(inputShape[3]);
+            OperationTests.CheckAllPermutations(verticalFlip, horizontalFlip, input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
+
+            Assert.IsTrue(verticalFlip.ChangeCoordinates());
+            Assert.IsTrue(horizontalFlip.ChangeCoordinates());
+        }
+
+
+        [Test]
+        public void TestRotate180Degrees()
+        {
+            //single element
+            var input = new[] { 12f };
+            var inputShape = new[] { 1, 1, 1, 1 };
+            var expected = new[] { 12f };
+            OperationTests.Check(new Rotate180Degrees(inputShape[2], inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
+
+            //single line
+            input = new[] { 12f, 13, 14 };
+            inputShape = new[] { 1, 1, 1, 3 };
+            expected = new[] { 14f, 13, 12 };
+            OperationTests.Check(new Rotate180Degrees(inputShape[2], inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeEnum.Nearest);
+
+            //single column
+            input = new[] { 12f, 13, 14 };
+            inputShape = new[] { 1, 1, 3, 1 };
+            expected = new[] { 14f, 13, 12 };
+            OperationTests.Check(new Rotate180Degrees(inputShape[2], inputShape[3]), input, inputShape, expected, null, ImageDataGenerator.FillModeE
