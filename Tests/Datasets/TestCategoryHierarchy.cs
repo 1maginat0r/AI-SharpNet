c3e73df05ecae87b861c4c82779572ecdc1b0dc6
1maginat0r <imaginai90@proton.me>
2023-05-05 08:32:48 +0000

    fix: Gemfile & Gemfile.lock to reduce vulnerabilities

--- Tests/Datasets/TestCategoryHierarchy.cs
+++ Tests/Datasets/TestCategoryHierarchy.cs
@@ -29,4 +29,43 @@ namespace SharpNetTests.Datasets
         }
 
 
-        [TestCase(new
+        [TestCase(new[] { "full" }, "etoile_pleine")]
+        [TestCase(new[] { "1digit"}, "etoile*")]
+        [TestCase(new[] { "1digit", "7" }, "etoile7")]
+        [TestCase(new[] { "2digits", "3", "*" }, "etoile3*")]
+        [TestCase(new[] { "2digits", "*", "7" }, "etoile*7")]
+        [TestCase(new[] { "2digits", "3", "7" }, "etoile37")]
+        [TestCase(new[] { "2digits"}, "etoile**")]
+        public void TestToPathForStars(string[] expectedPath, string cancelName)
+        {
+            Assert.AreEqual(expectedPath, ToPathForStars(cancelName));
+        }
+
+        private static string[] ToPathForStars(string cancelName)
+        {
+            if (cancelName.StartsWith("etoile_pleine")) {return new[] {"full" };}
+            if (cancelName.Equals("etoile*")) {return new[] {"1digit" };}
+            if (cancelName.Equals("etoile**")) {return new[] {"2digits" };}
+            switch (cancelName.Length)
+            {
+                case 7: return new[] {"1digit", cancelName[6].ToString()};
+                case 8: return new[] {"2digits", cancelName[6].ToString(), cancelName[7].ToString() };
+                default: return null;
+            }
+        }
+
+        [Test]
+        public void TestGetExpected()
+        {
+            var stars = StarExample();
+            Assert.IsTrue(Utils.SameContent(new float[] { -300, 0, 21, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  
+                stars.ExpectedPrediction(new string[] { }), 1e-6));
+            Assert.IsTrue(Utils.SameContent(new float[] { 30, 0, -160, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -100, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                stars.ExpectedPrediction(new [] {"full" }), 1e-6));
+            Assert.IsTrue(Utils.SameContent(new float[] { 30, 0, -160, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                stars.ExpectedPrediction(new [] {"1digit" }), 1e-6));
+            Assert.IsTrue(Utils.SameContent(new float[] { 30, 0, -160, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 91, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                stars.ExpectedPrediction(new [] {"1digit", "5" }), 1e-6));
+            Assert.IsTrue(Utils.SameContent(new float[] { 30, 0, -160, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                stars.ExpectedPrediction(new[] { "1digit", "*" }), 1e-6));
+            Assert.IsTrue(Utils.
