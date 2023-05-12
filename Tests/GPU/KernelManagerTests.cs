8efa7bad18cccfcb72b89b47d354f0b49d112832
1maginat0r <imaginai90@proton.me>
2023-05-12 18:10:27 +0000

    npm deploy

--- Tests/GPU/KernelManagerTests.cs
+++ Tests/GPU/KernelManagerTests.cs
@@ -44,4 +44,45 @@ namespace SharpNetTests.GPU
         {
             var cudaSrcCode =
                 " //  y = (x-mean)/volatility" + Environment.NewLine +
-                " __global__ void StandardizeInPlaceByRow(int N, int col
+                " __global__ void StandardizeInPlaceByRow(int N, int cols, int pointsByThread, int threadsByRow, float* __restrict x, float* __restrict row_mean, float* __restrict row_variance, float epsilon)"+ Environment.NewLine +
+                "{int idx = blockIdx.x * blockDim.x + threadIdx.x;}" + Environment.NewLine +
+                " __device__ inline float sigmoidf(float x) { return 1.0f / (1 + expf(-x)); "+ Environment.NewLine +
+                " }" + Environment.NewLine + "   __global__ void " + Environment.NewLine +
+                "numpy_sum_RowByRow(int rows, int cols, const float* __restrict x, float* __restrict sum_buffer) " + Environment.NewLine +
+                "{ " + Environment.NewLine +
+                " //test" + Environment.NewLine +
+                " } " + Environment.NewLine;
+            var observedResult = KernelManager.ExtractCudaKernelNames(cudaSrcCode);
+            var expectedResult = new System.Collections.Generic.List<string>{ "StandardizeInPlaceByRow", "numpy_sum_RowByRow" };
+            Assert.AreEqual(expectedResult, observedResult);
+        }
+
+        [Test]
+        public void TestReplaceTemplateWithTargetTypeName()
+        {
+            var srcCode = " void A(int N, T cols, T* pointsByThread, aT threadsByRow, float* T1)";
+            var observedResult = KernelManager.ReplaceTemplateWithTargetTypeName(srcCode, "double");
+            const string expectedResult = " void A(int N, double cols, double* pointsByThread, aT threadsByRow, float* T1)";
+            Assert.AreEqual(expectedResult, observedResult);
+        }
+        
+
+        [TestCase(1,1,0,2048,20,32)]
+        [TestCase(1,1,1,2048, 20, 32)]
+        [TestCase(1,30,30,2048, 20, 32)]
+        [TestCase(20,128,2047,2048, 20, 32)]
+        [TestCase(20,224,2*2048-1,2048, 20, 32)]
+        [TestCase(20, 2048, 20 * 2047, 2048, 20, 32)]
+        [TestCase(20,2048,20*2048,2048, 20, 32)]
+        [TestCase(21,2048,20*2049,2048, 20, 32)]
+        [TestCase(524288, 2048, 1024*1024*1024, 2048, 20, 32)]
+        [TestCase(524288+1, 2048, 1024*1024*1024+1, 2048, 20, 32)]
+        public void Compute_BlocksPerGrid_ThreadsPerBlockTest(int expectedBlocksPerGrid, int expectedThreadsPerBlock, int count, int maxThreadsPerBlock, int multiProcessorCount, int warpSize)
+        {
+            var (BlocksPerGrid, ThreadsPerBlock) = KernelManager.Compute_BlocksPerGrid_ThreadsPerBlock(count, maxThreadsPerBlock, multiProcessorCount, warpSize,256);
+            Assert.AreEqual(expectedBlocksPerGrid, BlocksPerGrid);
+            Assert.AreEqual(expectedThreadsPerBlock, ThreadsPerBlock);
+        }
+
+        [Test, Explicit]
+     
